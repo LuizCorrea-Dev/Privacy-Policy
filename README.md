a7001cd422d4165105e3925c2d0e5b0d1fd1463b
Luiz Correa <63646335+LuizCorrea-Dev@users.noreply.github.com>
2023-03-15 08:56:19 -0300

    primeira versao

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Privacy-Policy
+
+1 - npm init -y
+
+2 - npm i next react react-dom
+
+5 -  "scripts": {
+        "dev": "next dev"
+      },
+
+6 - para rodar localmente é : npm run dev
+
+
+-- para deploy --
+
+1 - parar o servidor de desenvolvimento
+2 - dar um git status (pega todas as alteraçoes)
+3 - e comitar na branch principal
+  git add -A
+  git commit -m 'primeira pagina'
+  git push origin main
---
+++ pages/index.js
@@ -0,0 +1,50 @@
+function Home () {
+    return<div>
+        <h2>Política de Privacidade</h2>
+        <p>Luiz Correa construiu o aplicativo como um aplicativo gratuito. Este SERVIÇO é fornecido por Luiz Correa sem custo e deve ser usado no estado em que se encontra.</p>
+        <p>Esta página é usada para informar os visitantes sobre as políticas com a coleta, uso e divulgação de Informações Pessoais se alguém decidir usar o meu Serviço.</p>
+        <p>Se você optar por usar o meu Serviço, você concorda com a coleta e uso de informações em relação a esta política. As Informações Pessoais que Eu coleto são usadas para fornecer e melhorar o Serviço. Eu não usarei ou compartilharei suas informações com ninguém, exceto conforme descrito nesta Política de Privacidade.</p>
+        <p>Os termos usados ​​nesta Política de Privacidade têm os mesmos significados que em nossos Termos e Condições, que podem ser acessados ​​em, a menos que definido de outra forma nesta Política de Privacidade.</p>
+
+        <h3>Coleta e uso de informações</h3>
+        <p>Para uma melhor experiência, ao usar meu Serviço, eu posso exigir que você nos forneça certas informações de identificação pessoal primeiro nome. As informações que eu solicito será retida em seu dispositivo e não coletada por mim de forma alguma.</p>
+        <p>O aplicativo usa serviços de terceiros que podem coletar informações usadas para identificá-lo.</p>
+        <p>Link para a política de privacidade de provedores de serviços terceirizados usados ​​pelo aplicativo</p>
+
+        <p><a href="https://policies.google.com/privacy">Serviços do Google Play</a></p>
+        <p><a href="https://support.google.com/admob/answer/6128543?hl=en">AdMob</a></p>
+
+        <h3>Dados de registro</h3>
+        <p>Quero informar que sempre que você usa meu Serviço, em caso de erro no aplicativo, eu coleto dados e informações (através de produtos de terceiros) em seu telefone chamado Log Data. Esses dados de registro podem incluir informações como endereço de protocolo de Internet ("IP") do dispositivo, nome do dispositivo, versão do sistema operacional, configuração do aplicativo ao utilizar meu serviço, hora e data de uso do serviço e outras estatísticas .</p>
+        <h3>Cookies</h3>
+        <p>Cookies são arquivos com uma pequena quantidade de dados que são comumente usados como identificadores únicos anônimos. Estes são enviados para o seu navegador a partir dos sites que você visita e são armazenados na memória interna do seu dispositivo.</p>
+        <p>Este Serviço não usa esses “cookies” explicitamente. No entanto, o aplicativo pode usar código e bibliotecas de terceiros que usam “cookies” para coletar informações e melhorar seus serviços. Você tem a opção de aceitar ou recusar esses cookies e saber quando um cookie está sendo enviado ao seu dispositivo. Se você optar por recusar nossos cookies, talvez não consiga usar algumas partes deste Serviço.</p>
+        <h3>Provedores de serviço</h3>
+        <p>Posso contratar empresas e indivíduos terceirizados pelos seguintes motivos:</p>
+        <ul>
+            <li>Para facilitar nosso Serviço;</li>
+            <li>Para fornecer o Serviço em nosso nome;</li>
+            <li>Para realizar serviços relacionados ao Serviço; ou</li>
+            <li>Para nos ajudar a analisar como nosso Serviço é usado.</li>
+        </ul>
+        <p>Desejo informar aos usuários deste Serviço que esses terceiros têm acesso às suas Informações Pessoais. O motivo é realizar as tarefas atribuídas a eles em nosso nome. No entanto, eles são obrigados a não divulgar ou usar as informações para qualquer outra finalidade.</p>
+        <h3>Segurança</h3>
+        <p>Eu valorizo sua confiança em nos fornecer suas informações pessoais, portanto, estamos nos esforçando para usar meios comercialmente aceitáveis de protegê-las. Mas lembre-se que nenhum método de transmissão pela internet, ou método de armazenamento eletrônico é 100% seguro e confiável, e não posso garantir sua segurança absoluta.</p>
+        <h3>Links para outros sites</h3>
+        <p>Este Serviço pode conter links para outros sites. Se você clicar em um link de terceiros, será direcionado para esse site. Observe que esses sites externos não são operados por mim. Portanto, aconselho fortemente que você revise a Política de Privacidade desses sites. Não tenho controle e não assumo nenhuma responsabilidade pelo conteúdo, políticas de privacidade ou práticas de sites ou serviços de terceiros.</p>
+        <h3>Privacidade das crianças</h3>
+        <p>Esses Serviços não se dirigem a menores de 13 anos. Não coleto intencionalmente informações de identificação pessoal de crianças menores de 13 anos. No caso de eu descobrir que uma criança menor de 13 anos me forneceu informações pessoais, eu as excluo imediatamente de nossos servidores. Se você é pai ou responsável e está ciente de que seu filho nos forneceu informações pessoais, entre em contato comigo para que eu possa tomar as medidas necessárias.</p>
+        <h3>Mudanças nesta Política de Privacidade</h3>
+        <p>Eu posso atualizar minha Política de Privacidade de tempos em tempos. Portanto, é recomendável consultar esta página periodicamente para verificar quaisquer alterações. Eu o notificarei sobre quaisquer alterações, publicando a nova Política de Privacidade nesta página.</p>
+        <p>Esta política está em vigor a partir de 15/03/2023</p>
+        <h3>Entre em contato comigo</h3>
+        <p>Se você tiver dúvidas ou sugestões sobre a minha Política de Privacidade, não hesite em entrar em contato com comigo.</p>
+
+
+        
+
+
+    </div>
+}
+
+export default Home
